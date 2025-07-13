7e43f6d9ae2ea4c8ca0009ee70278142d01284ba
umanagendrakumar <your.email@example.com>
2025-07-13 17:14:29 +0530

    Border Removed

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -2,7 +2,7 @@ import { FaRegFaceMehBlank } from "react-icons/fa6";
 
 const Empty = ({ value }) => {
     return (
-        <div className="p-8 border rounded w-full max-w-sm text-center">
+        <div className="p-8 rounded w-full max-w-sm text-center">
             <FaRegFaceMehBlank className="text-6xl mx-auto my-4" />
             <h1 className="text-2xl font-bold"> <span className="text-red-400">NO</span> {value}</h1>
         </div>
