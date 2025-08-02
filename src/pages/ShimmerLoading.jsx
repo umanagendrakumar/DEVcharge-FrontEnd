26b722e98c92f0ccdb6d55069683b41920d43076
umanagendrakumar <your.email@example.com>
2025-08-02 22:20:46 +0530

    Brought div to inside of other div.

--- src/pages/ShimmerLoading.jsx
+++ src/pages/ShimmerLoading.jsx
@@ -3,15 +3,13 @@ import "./ShimmerAnim.css"; // Only the animation
 const ShimmerLoading = () => {
     return (
         <div className="w-full p-4">
-            {[...Array(5)].map((_, index) => (
-                <div key={index} className="flex gap-4 justify-center items-center mb-4">
-
-                    <div className="relative overflow-hidden w-16 h-16 rounded-full bg-base-100">
-                        <div className="absolute inset-0 shimmer"></div>
-                    </div>
-
+            {[...Array(4)].map((_, index) => (
+                <div key={index} className="flex gap-4 justify-center  mb-4">
                     <div className="relative overflow-hidden w-full max-w-2xl h-20 bg-base-100 rounded">
-                        <div className="absolute inset-0 shimmer"></div>
+                        <div className="absolute inset-0 shimmer px-4 flex items-center">
+                            <div className="w-15 h-15 rounded-full bg-gray-600">
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
