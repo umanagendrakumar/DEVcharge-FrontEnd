640398cc2a79fdb9e7726a364212c725947a70d9
umanagendrakumar <your.email@example.com>
2025-08-04 20:22:02 +0530

    border added for effect

--- src/pages/ShimmerLoading.jsx
+++ src/pages/ShimmerLoading.jsx
@@ -5,7 +5,7 @@ const ShimmerLoading = () => {
         <div className="w-full p-4">
             {[...Array(4)].map((_, index) => (
                 <div key={index} className="flex gap-4 justify-center  mb-4">
-                    <div className="relative overflow-hidden w-full max-w-2xl h-20 bg-base-100 rounded">
+                    <div className="relative overflow-hidden w-full max-w-2xl h-20 border border-cyan-500 bg-base-100 rounded">
                         <div className="absolute inset-0 shimmer px-4 flex items-center">
                             <div className="w-15 h-15 rounded-full bg-gray-600">
                             </div>
